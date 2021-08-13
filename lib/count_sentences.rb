e4ebb5e14bf1142d745490e858f6ba9e8de6f7a8
Trace <tracedelange@me.com>
2021-08-13 11:09:36 -0500

    commit tree eb6282fd759a708127268944e6ee1d5fe0b8d3f9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,39 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[self.length-1] == '.'
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self[self.length-1] == '?'
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self[self.length-1] == '!'
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    final = []
+    self.split('. ').each do |substring|
+      substring.split('! ').each do |subsubstring|
+        (subsubstring.split('? ')).each do |lastsub|
+          final.push(lastsub)
+        end
+      end
+    end
+    return final.length
   end
+  
 end
